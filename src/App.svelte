4d4dd04ac401480429ee640e0115c9ed1f4ba7ab
bbrassart <brassart@gmail.com>
2023-04-27 17:17:09 +0200

    Second try for GH pages

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex flex-wrap p-6">
 	<div class="w-full text-center mt-8">
-		<h1 class="text-4xl mb-6">Welcome to my app</h1>
+		<h1 class="text-4xl mb-6 text-red">Welcome to my app</h1>
 		<p class="mb-2">Testing Svelte with TailwindCSS</p>
 	</div>
 	<Nested />
