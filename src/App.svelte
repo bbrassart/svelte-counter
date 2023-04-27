a656164c057b9b842dae36a46df6974e47d35021
bbrassart <brassart@gmail.com>
2023-04-27 16:23:43 +0200

    Counter svelte: Add TailwindCSS

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Nested from "./lib/components/Nested.svelte";
+</script>
+
+<div class="flex flex-wrap p-6">
+	<div class="w-full text-center mt-8">
+		<h1 class="text-4xl mb-6">Welcome to my app</h1>
+		<p class="mb-2">Testing Svelte with TailwindCSS</p>
+	</div>
+	<Nested />
+</div>
